d0e17cb24a349187cb17d4a47a3a2e437f33143d
雨生 <354801380@qq.com>
2025-10-02 13:35:58 +0800

    fix: 移除多余模块

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -22,55 +22,10 @@ export const navItems: NavItem[] = [
     items: [] // Empty array as there are no child items for Dashboard
   },
   {
-    title: 'Product',
-    url: '/dashboard/product',
-    icon: 'product',
-    shortcut: ['p', 'p'],
-    isActive: false,
-    items: [] // No child items
-  },
-  {
     title: 'Tasks',
     url: '/dashboard/tasks',
     icon: 'checklist',
     shortcut: ['t', 't'],
-    isActive: false,
-    items: [] // No child items
-  },
-  {
-    title: 'Account',
-    url: '#', // Placeholder as there is no direct link for the parent
-    icon: 'billing',
-    isActive: true,
-
-    items: [
-      {
-        title: 'Profile',
-        url: '/dashboard/profile',
-        icon: 'userPen',
-        shortcut: ['m', 'm']
-      },
-      {
-        title: 'Login',
-        shortcut: ['l', 'l'],
-        url: '/',
-        icon: 'login'
-      }
-    ]
-  },
-  {
-    title: 'Kanban',
-    url: '/dashboard/kanban',
-    icon: 'kanban',
-    shortcut: ['k', 'k'],
-    isActive: false,
-    items: [] // No child items
-  },
-  {
-    title: 'Chat',
-    url: '/dashboard/chat',
-    icon: 'messageSquare',
-    shortcut: ['c', 'c'],
     isActive: false,
     items: [] // No child items
   }
